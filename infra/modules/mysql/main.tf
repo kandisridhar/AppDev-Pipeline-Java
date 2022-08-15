50f07f24a599635bfebd23cbf9c29a1077c7d21b
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-15 18:24:20 +0530

    Update main.tf

--- infra/modules/mysql/main.tf
+++ infra/modules/mysql/main.tf
@@ -1,17 +1,17 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
-  }
-}
-
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  skip_provider_registration = true
-  features {}
-}
+#terraform {
+#  required_providers {
+#    azurerm = {
+#      source  = "hashicorp/azurerm"
+#      version = "=3.0.0"
+#    }
+#  }
+#}
+#
+## Configure the Microsoft Azure Provider
+#provider "azurerm" {
+#  skip_provider_registration = true
+#  features {}
+#}
 
 
 
