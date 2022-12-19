41008047b51aeb5e9646f75890bbb326f05348a7
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-12-19 16:02:59 +0530

    Update main.tf

--- infra/modules/acr/main.tf
+++ infra/modules/acr/main.tf
@@ -14,9 +14,5 @@ resource "azurerm_container_registry" "acr" {
     zone_redundancy_enabled = true
     tags                    = {}
   }
-  georeplications {
-    location                = "North Europe"
-    zone_redundancy_enabled = true
-    tags                    = {}
-  }
+ 
 }
