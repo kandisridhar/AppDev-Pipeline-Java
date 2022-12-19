271d11ac7a34d108a051606420d03c8416f36447
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-12-19 16:05:44 +0530

    Update main.tf

--- infra/modules/acr/main.tf
+++ infra/modules/acr/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_resource_group" "main" {
   location = "East US"
 }
 
-resource "azurerm_container_registry" "acr" {
+resource "azurerm_container_registry" "main" {
   name                = "articontainer01"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
